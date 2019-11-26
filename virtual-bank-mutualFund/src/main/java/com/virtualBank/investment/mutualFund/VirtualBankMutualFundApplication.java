22d22879007d3ce4f94188bcdc25a66a50954bf3
a5365958 <2571706435@qq.com>
2019-11-26 08:53:47 +0800

    Add files via upload

---
+++ virtual-bank-mutualFund/src/main/java/com/virtualBank/investment/mutualFund/VirtualBankMutualFundApplication.java
@@ -0,0 +1,13 @@
+package com.virtualBank.investment.mutualFund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualBankMutualFundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualBankMutualFundApplication.class, args);
+	}
+
+}
